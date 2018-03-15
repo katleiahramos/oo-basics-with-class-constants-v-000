741132ba59dd4f3f7a355b06233d3cc19917e5e7
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-15 18:56:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,9 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
+  #def initialize(brand)
+  #  @brand = brand
+#  end
   BRANDS = []
 
   def cobble
